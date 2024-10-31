fcd1639cebc26771c8bc7f46b9fcf5ec63b3f792
TheCGuyGitHub <christimaibach1@gmail.com>
2024-10-31 23:20:53 +0100

    Remove unused values and Imports in VelocityRegistrationHandler

--- VulpesCloud-connector/src/main/kotlin/io/github/thecguygithub/connector/velocity/VelocityRegistrationHandler.kt
+++ VulpesCloud-connector/src/main/kotlin/io/github/thecguygithub/connector/velocity/VelocityRegistrationHandler.kt
@@ -1,25 +1,23 @@
 package io.github.thecguygithub.connector.velocity
 
-import com.velocitypowered.api.proxy.server.RegisteredServer
 import com.velocitypowered.api.proxy.server.ServerInfo
-import io.github.thecguygithub.node.networking.redis.RedisManager
 import java.net.InetSocketAddress
 
 object VelocityRegistrationHandler {
 
-    private val redis = VelocityConnector.instance.wrapper.getRC()
-    private val redisManager = redis?.let { RedisManager(it.getJedisPool()) }
-    private val redisChannels = listOf("vulpescloud-action-service")
     private val proxyServer = VelocityConnector.instance.proxyServer
 
     init {
         for (server in servers()) {
             proxyServer.getServer(server).ifPresent { proxyServer.unregisterServer(it.serverInfo) }
         }
+
+        servers().forEach { println(it) }
     }
 
     fun registerServer(name: String, address: String, port: Int) {
         proxyServer.registerServer(ServerInfo(name, InetSocketAddress(address, port)))
+        servers().forEach { println(it) }
     }
 
     fun unregisterServer(name: String) {
@@ -28,6 +26,7 @@ object VelocityRegistrationHandler {
                 it.serverInfo
             )
         }
+        servers().forEach { println(it) }
     }
 
     fun servers(): List<String> {
