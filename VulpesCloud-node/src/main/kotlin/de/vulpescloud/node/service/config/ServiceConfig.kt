575f7bb59df1cee134c9382a964e6ac7d8335c7d
TheCGuyGitHub <christimaibach1@gmail.com>
2024-11-11 19:33:25 +0100

    Removed debug messages from config

--- VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/service/config/ServiceConfig.kt
+++ VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/service/config/ServiceConfig.kt
@@ -10,21 +10,12 @@ import java.nio.file.Files
 object ServiceConfig {
 
     fun makeServiceConfigs(service: LocalService) {
-
-        val logger = Logger()
-
-        logger.warn("making Config")
-        logger.warn("Version: ${service.task.version().name}")
-
         when (service.task.version().name) {
             "velocity" -> {
-                logger.warn("VELOCITY CHOSEN")
-                logger.warn("file: ${service.runningDir.resolve("velocity.toml")}")
                 // Copy the Config
                 if (!Files.exists(service.runningDir.resolve("velocity.toml"))) {
                     FileSystemUtil.copyClassPathFile(this::class.java.classLoader, "platforms/Velocity/velocity.toml", "${service.runningDir.resolve("velocity.toml")}")
                 }
-                logger.warn("File copied")
 
                 // Load the Config
                 val velocityConfig = FileConfig.builder(service.runningDir.resolve("velocity.toml"), TomlFormat.instance())
@@ -33,17 +24,12 @@ object ServiceConfig {
                     .defaultData(this::class.java.classLoader.getResource("platforms/Velocity/velocity.toml"))
                     .build()
                 velocityConfig.load()
-                logger.warn("Loaded")
 
                 // Set the Stuff in the Config
                 velocityConfig.set<String>("bind", service.hostname() + ":" + service.port())
 
-                logger.warn("set!")
-
                 // save the config
                 velocityConfig.save()
-
-                logger.warn("DONE")
             }
         }
     }
