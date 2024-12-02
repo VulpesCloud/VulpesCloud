10f077bf8beb4d7fb9df369578806b85ab4930aa
TheCGuyGitHub <christimaibach1@gmail.com>
2024-12-02 17:31:37 +0100

    fix: The build shouldn't fail now

--- VulpesCloud-modules/VulpesCloud-example-module/src/main/kotlin/de/vulpescloud/modules/example/ExampleModule.kt
+++ VulpesCloud-modules/VulpesCloud-example-module/src/main/kotlin/de/vulpescloud/modules/example/ExampleModule.kt
@@ -27,14 +27,14 @@ package de.vulpescloud.modules.example
 import de.vulpescloud.api.module.VulpesModule
 import org.slf4j.LoggerFactory
 
-class ExampleModule : VulpesModule {
-    private val logger = LoggerFactory.getLogger(ExampleModule::class.java)
-
-    override fun enable() {
-        logger.info("Enabled ExampleModule! :>")
-    }
-
-    override fun disable() {
-        logger.info("Disabled ExampleModule! :<")
-    }
-}
+//class ExampleModule : VulpesModule {
+//    private val logger = LoggerFactory.getLogger(ExampleModule::class.java)
+//
+//    override fun enable() {
+//        logger.info("Enabled ExampleModule! :>")
+//    }
+//
+//    override fun disable() {
+//        logger.info("Disabled ExampleModule! :<")
+//    }
+//}
