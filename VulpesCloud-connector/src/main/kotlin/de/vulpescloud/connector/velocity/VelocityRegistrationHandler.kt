fd5567c8c5d953a8b81cae66ec103f9d3c02de18
TheCGuyGitHub <christimaibach1@gmail.com>
2024-12-14 19:32:19 +0100

    Services now register all servers that are already online

--- VulpesCloud-connector/src/main/kotlin/de/vulpescloud/connector/velocity/VelocityRegistrationHandler.kt
+++ VulpesCloud-connector/src/main/kotlin/de/vulpescloud/connector/velocity/VelocityRegistrationHandler.kt
@@ -1,6 +1,9 @@
 package de.vulpescloud.connector.velocity
 
 import com.velocitypowered.api.proxy.server.ServerInfo
+import de.vulpescloud.api.services.ServiceFilters
+import de.vulpescloud.api.services.ServiceStates
+import de.vulpescloud.bridge.service.ServiceProvider
 import de.vulpescloud.wrapper.Wrapper
 import java.net.InetSocketAddress
 
@@ -9,18 +12,21 @@ object VelocityRegistrationHandler {
     private val proxyServer = VelocityConnector.instance.proxyServer
 
     init {
-        servers().forEach { println(it) }
         for (server in servers()) {
             proxyServer.getServer(server).ifPresent { proxyServer.unregisterServer(it.serverInfo) }
         }
 
-        servers().forEach { println(it) }
+        ServiceProvider.findServicesByFilter(ServiceFilters.SERVERS)?.forEach {
+            if (it.state() == ServiceStates.ONLINE) {
+                this.registerServer(it.name(), it.hostname(), it.port())
+            }
+        }
+
     }
 
     fun registerServer(name: String, address: String, port: Int) {
         Wrapper.instance.getRC()?.sendMessage("Registering -> $name, $address:$port", "test_return")
         proxyServer.registerServer(ServerInfo(name, InetSocketAddress(address, port)))
-        proxyServer.allServers.forEach { println("srv ->" + it.serverInfo.name) }
     }
 
     fun unregisterServer(name: String) {
