3b6ce4e29e5ddc049a8c6370f0b8c6ff9ed4940f
TheCGuyGitHub <christimaibach1@gmail.com>
2025-01-24 17:12:19 +0100

    chore: Update exposed version in Launcher

--- VulpesCloud-launcher/src/main/kotlin/de/vulpescloud/launcher/VulpesLauncher.kt
+++ VulpesCloud-launcher/src/main/kotlin/de/vulpescloud/launcher/VulpesLauncher.kt
@@ -67,8 +67,8 @@ class VulpesLauncher {
             val yamlConfig = Dependency("com.electronwill.night-config", "yaml", "3.8.1")
             val tomlConfig = Dependency("com.electronwill.night-config", "toml", "3.8.1")
             val coreConfig = Dependency("com.electronwill.night-config", "core", "3.8.1")
-            val exposedCore = Dependency("org.jetbrains.exposed", "exposed-core", "0.57.0")
-            val exposedJDBC = Dependency("org.jetbrains.exposed", "exposed-jdbc", "0.57.0")
+            val exposedCore = Dependency("org.jetbrains.exposed", "exposed-core", "0.58.0")
+            val exposedJDBC = Dependency("org.jetbrains.exposed", "exposed-jdbc", "0.58.0")
             val snakeYAML = Dependency("org.yaml", "snakeyaml", "2.3")
 
             // Downloading the Dependency's
