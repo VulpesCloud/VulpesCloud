9ccbea5ae4a2e423fb48c4bfc846e65dbd2ba3af
TheCGuyGitHub <christimaibach1@gmail.com>
2025-02-02 14:09:48 +0100

    Add node Updater

---
+++ VulpesCloud-launcher/src/main/kotlin/de/vulpescloud/launcher/updater/NodeUpdater.kt
@@ -0,0 +1,20 @@
+package de.vulpescloud.launcher.updater
+
+import de.vulpescloud.launcher.VulpesLauncher.Companion.config
+import de.vulpescloud.launcher.VulpesLauncher.Companion.githubURL
+import de.vulpescloud.launcher.util.ChecksumUtil
+import de.vulpescloud.launcher.util.FileUpdaterUtil
+import java.io.File
+import java.net.URI
+
+class NodeUpdater {
+
+    fun updateNode() {
+        val wantedChecksum = ChecksumUtil.returnChecksumJson().getString("node")
+        val target = File("launcher/dependencies/vulpescloud-node.jar")
+        val downloadUri = URI(githubURL + config.autoUpdatesBranch() + "/vulpescloud-node.jar")
+
+        FileUpdaterUtil.updateFile(target, downloadUri, wantedChecksum)
+    }
+
+}
