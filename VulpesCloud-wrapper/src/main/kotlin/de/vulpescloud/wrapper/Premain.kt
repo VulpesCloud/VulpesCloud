102c65412fe8f8d77129cbc88ef00f71df52981e
TheCGuyGitHub <christimaibach1@gmail.com>
2025-03-07 09:20:31 +0100

    cleanup premain

--- VulpesCloud-wrapper/src/main/kotlin/de/vulpescloud/wrapper/Premain.kt
+++ VulpesCloud-wrapper/src/main/kotlin/de/vulpescloud/wrapper/Premain.kt
@@ -26,15 +26,12 @@ package de.vulpescloud.wrapper
 
 import java.lang.instrument.Instrumentation
 
-
-
 object Premain {
 
     lateinit var INSTRUMENTATION: Instrumentation
 
     @JvmStatic
     fun premain(args: String?, instrumentation: Instrumentation) {
-        println("premain yay")
         INSTRUMENTATION = instrumentation
     }
 
