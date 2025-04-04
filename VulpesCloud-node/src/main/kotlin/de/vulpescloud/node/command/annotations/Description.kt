f256bdb759967d3ac0e6860191abb145289a0467
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-04-04 17:41:15 +0200

    Remove everything from the node

--- VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/command/annotations/Description.kt
+++
@@ -1,9 +0,0 @@
-package de.vulpescloud.node.command.annotations
-
-
-@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class Description(
-    val description: String,
-    val translatable: Boolean = true
-)
