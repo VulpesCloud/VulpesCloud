9c2f6360ac8294c720d5b7f13f6ad15100b5c7f8
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-04-11 21:14:27 +0200

    Get the jarFile and copy the files from it

--- VulpesCloud-api/src/main/kotlin/de/vulpescloud/api/lang/Translator.kt
+++ VulpesCloud-api/src/main/kotlin/de/vulpescloud/api/lang/Translator.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.launch
 import java.nio.file.Paths
 import org.json.JSONObject
 import org.slf4j.LoggerFactory
+import java.net.JarURLConnection
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 import kotlin.io.path.name
@@ -31,16 +32,29 @@ class Translator {
 
             logger.info("Loading files for Language &m${lang.name}")
 
-            val res = this::class.java.classLoader.getResource("lang/${lang.name}")
-
-            logger.debug("${res.path};;;${res.protocol};;;$res")
-
-            this::class.java.classLoader.getResourceAsStream("lang/${lang.name}").let {
-                if (it == null) {
-                    logger.error("Cannot get Language data, InputStream is null, check location of Language Data")
+            this::class.java.classLoader.getResource("lang/${lang.name}").let { url ->
+                if (url == null) {
+                    logger.error("Cannot get Language data, URL is null, check location of Language Data")
                     return@launch
                 }
-                Files.copy(it, finalLangDir.resolve("test"), StandardCopyOption.REPLACE_EXISTING)
+                if (url.protocol != "jar") {
+                    logger.error("Protocol is not 'jar', i don't really know why this could happen, but anyways")
+                    return@launch
+                }
+                val connection = url.openConnection() as JarURLConnection
+                val jarFile = connection.jarFile
+
+                jarFile.entries().asSequence()
+                    .filter { it.name.startsWith("lang/${lang.name}") && !it.isDirectory }
+                    .forEach { entry ->
+                        val relativePath = entry.name.removePrefix("lang/${lang.name}")
+                        val outPath = finalLangDir.resolve(relativePath)
+                        jarFile.getInputStream(entry).use { input ->
+                            Files.createDirectories(outPath.parent)
+                            Files.copy(input, outPath, StandardCopyOption.REPLACE_EXISTING)
+                            logger.debug("Copied file: &m${entry.name}")
+                        }
+                    }
             }
             Files.walk(finalLangDir).filter { it.toString().endsWith(".json") }.forEach {
                 logger.debug("Loading JSON from file: &m${it.name}")
