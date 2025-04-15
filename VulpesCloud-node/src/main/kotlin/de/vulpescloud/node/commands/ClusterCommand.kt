e635023a04a10bb6f5e77a509c85d150ef583dce
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-04-15 11:53:31 +0200

    Add cluster list command

---
+++ VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/commands/ClusterCommand.kt
@@ -0,0 +1,22 @@
+package de.vulpescloud.node.commands
+
+import de.vulpescloud.api.cluster.ClusterProvider
+import de.vulpescloud.node.command.CommandSource
+import org.incendo.cloud.annotations.Command
+
+@Suppress("Unused")
+class ClusterCommand(
+    val clusterProvider: ClusterProvider
+) {
+
+    @Command("cluster list")
+    fun displayClusterInfo(
+        source: CommandSource
+    ) {
+        source.sendMessage("Showing &m${clusterProvider.nodes().size} &7nodes&8:")
+        clusterProvider.nodes().forEach {
+            source.sendMessage(" &8- &m${it.name} &7Version&8:<yellow>${it.cloudVersion} &7HeadNode&8:<yellow>${it.isHeadNode}")
+        }
+    }
+
+}
