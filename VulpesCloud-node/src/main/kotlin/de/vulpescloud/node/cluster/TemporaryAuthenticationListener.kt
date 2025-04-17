2a23604a187d5f971f206365adbc7adcd1f01ed3
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-04-17 10:54:22 +0200

    fix: update TemporaryAuthenticationListener to call the event with the new method

--- VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/cluster/TemporaryAuthenticationListener.kt
+++ VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/cluster/TemporaryAuthenticationListener.kt
@@ -4,19 +4,20 @@ import de.vulpescloud.api.cluster.ClusterNode
 import de.vulpescloud.api.cluster.NodeStates
 import de.vulpescloud.api.event.EventManager
 import de.vulpescloud.api.event.events.cluster.NodeStateChangeEvent
+import de.vulpescloud.api.redis.RedisChannels
 import de.vulpescloud.jediswrapper.JedisWrapper.getRC
 import de.vulpescloud.jediswrapper.redis.ChannelListener
 import de.vulpescloud.node.NodeShutdown
 import de.vulpescloud.node.config.NodeConfig
+import de.vulpescloud.node.event.EventManagerImpl
 import org.json.JSONObject
 import org.slf4j.LoggerFactory
 
-class TemporaryAuthenticationListener(
-    private val config: NodeConfig,
-    private val eventManager: EventManager,
-) : ChannelListener("VULPESCLOUD_NODEAUTHENTICATION_${config.name()}") {
+class TemporaryAuthenticationListener(private val config: NodeConfig, eventManager: EventManager) :
+    ChannelListener("VULPESCLOUD_NODEAUTHENTICATION_${config.name()}") {
 
     private val logger = LoggerFactory.getLogger(TemporaryAuthenticationListener::class.java)
+    private val eventManager = eventManager as EventManagerImpl
 
     override fun onMessage(message: String) {
         val msg = JSONObject(JSONObject(message).getString("message"))
@@ -36,7 +37,10 @@ class TemporaryAuthenticationListener(
                     config.hostname(),
                 )
             getRC()?.setHashField("VULPESCLOUD_NODES", config.name(), JSONObject(node).toString())
-            eventManager.call(NodeStateChangeEvent(node, NodeStates.OFFLINE, NodeStates.BOOTING))
+            eventManager.callGlobal(
+                NodeStateChangeEvent(node, NodeStates.OFFLINE, NodeStates.BOOTING),
+                RedisChannels.VULPESCLOUD_EVENT_CLUSTER_NodeStateChangeEvent,
+            )
 
             this.unregister()
         } else {
