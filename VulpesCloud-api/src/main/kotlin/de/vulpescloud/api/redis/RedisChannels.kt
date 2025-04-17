b9a73527f2352fe7f3c31ab0adc6b11e7d3ca1a0
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-04-17 15:48:36 +0200

    Add more Redis channels

--- VulpesCloud-api/src/main/kotlin/de/vulpescloud/api/redis/RedisChannels.kt
+++ VulpesCloud-api/src/main/kotlin/de/vulpescloud/api/redis/RedisChannels.kt
@@ -2,6 +2,9 @@ package de.vulpescloud.api.redis
 
 enum class RedisChannels {
 
-    VULPESCLOUD_EVENT_CLUSTER_NodeStateChangeEvent;
+    VULPESCLOUD_EVENT_CLUSTER_NodeStateChangeEvent,
+    VULPESCLOUD_EVENT_MODULE_ModuleLoadEvent,
+    VULPESCLOUD_EVENT_MODULE_ModuleStartEvent,
+    VULPESCLOUD_EVENT_MODULE_ModuleUnloadEvent
 
 }
