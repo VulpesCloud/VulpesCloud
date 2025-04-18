7503affdf73f8623eb51874abc4d9b27667f1a90
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-04-18 23:40:32 +0200

    Add logging to shut down process

--- VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/NodeShutdown.kt
+++ VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/NodeShutdown.kt
@@ -10,9 +10,11 @@ import de.vulpescloud.node.terminal.JLineTerminal
 import kotlin.system.exitProcess
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import org.slf4j.LoggerFactory
 
 object NodeShutdown : KoinComponent {
 
+    private val logger = LoggerFactory.getLogger(NodeShutdown::class.java)
     private val terminal: JLineTerminal by inject()
     private val clusterProvider: ClusterProvider by inject()
     private val moduleProvider: ModuleProvider by inject()
@@ -23,19 +25,24 @@ object NodeShutdown : KoinComponent {
     }
 
     fun commandShutdown() {
-
+        logger.debug("Canceling Schedulers")
         if (ClusterHeartbeatScheduler.instance.isActive()) ClusterHeartbeatScheduler.instance.cancel()
         if (HeadNodeClusterHeartbeatScheduler.instance.isActive()) HeadNodeClusterHeartbeatScheduler.instance.cancel()
 
+        logger.debug("Deleting Heartbeat")
         getRC()?.deleteHashField("VULPESCLOUD_NODE_HEARTBEAT", clusterProvider.localNode().name)
 
+        logger.debug("Unloading Modules")
         moduleProvider.unloadAllModules()
 
+        logger.debug("Shutting down ClusterProvider")
         val clusterProv = clusterProvider as ClusterProviderImpl
         clusterProv.shutdown()
 
+        logger.debug("Shutting down RedisController")
         getRC()?.shutdown()
 
+        logger.info("Goodbye!")
         terminal.close()
         exitProcess(0)
     }
