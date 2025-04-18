f023b785393c49642d5d5cd6c6224c0e0d124c96
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-04-18 19:21:40 +0200

    Add Task data class

---
+++ VulpesCloud-api/src/main/kotlin/de/vulpescloud/api/task/Task.kt
@@ -0,0 +1,21 @@
+package de.vulpescloud.api.task
+
+import de.vulpescloud.api.service.Service
+import de.vulpescloud.api.template.Template
+import de.vulpescloud.api.version.Version
+
+data class Task(
+    val name: String,
+    val nodes: List<String>,
+    val templates: List<Template>,
+    val maxMemory: Int,
+    val maxPlayers: Int,
+    val staticServices: Boolean,
+    val minOnlineCount: Int,
+    val serviceCount: Int,
+    val services: List<Service>,
+    val maintenance: Boolean,
+    val startPort: Int,
+    val fallback: Boolean,
+    val version: Version
+)
