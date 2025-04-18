f0c09f285a269a87488f911873c78c1349a60011
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-04-18 14:07:49 +0200

    fix: rename stuff

--- VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/commands/ClusterCommand.kt
+++ VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/commands/ClusterCommand.kt
@@ -15,7 +15,7 @@ class ClusterCommand(
     ) {
         source.sendMessage("Showing &m${clusterProvider.nodes().size} &7nodes&8:")
         clusterProvider.nodes().forEach {
-            source.sendMessage(" &8- &m${it.name} &7Version&8:<yellow>${it.cloudVersion} &7HeadNode&8:<yellow>${it.isHeadNode}")
+            source.sendMessage(" &8- &m${it.name} &7Version&8:<yellow>${it.cloudVersion} &7HeadNode&8:<yellow>${it.headNode}")
         }
     }
 
