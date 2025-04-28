c4bf75ed5c20700e9bcf7ae8d981f4507576bc9c
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-04-28 15:36:27 +0200

    Add debug logging

--- VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/cluster/NodeCommunicationChannelListener.kt
+++ VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/cluster/NodeCommunicationChannelListener.kt
@@ -5,10 +5,14 @@ import de.vulpescloud.api.redis.RedisChannels
 import de.vulpescloud.jediswrapper.redis.ChannelListener
 import de.vulpescloud.node.service.NodeCommunicationServiceHandler.handleServiceMessage
 import de.vulpescloud.node.utils.JsonUtils.parsePubSubMessage
+import org.slf4j.LoggerFactory
 
 class NodeCommunicationChannelListener(private val clusterProvider: ClusterProvider) : ChannelListener(RedisChannels.VULPESCLOUD_NODE_COMMUNICATION.name) {
     override fun onMessage(message: String) {
         val msg = parsePubSubMessage(message)
+
+        LoggerFactory.getLogger("TempLogger")
+            .debug("Triggered NodeCommunicationChannelListener with message: {}, {}", msg, msg.getString("content"))
 
         when (msg.getString("content")) {
             "SERVICE" -> {
