726f05e56b5fb3ae1ac19560022e2c9585020cf7
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-05-04 21:04:05 +0200

    changes to NodeShutdown

--- VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/NodeShutdown.kt
+++ VulpesCloud-node/src/main/kotlin/de/vulpescloud/node/NodeShutdown.kt
@@ -9,8 +9,10 @@ import de.vulpescloud.node.mysql.DatabaseProvider
 import de.vulpescloud.node.service.ServiceProviderImpl
 import de.vulpescloud.node.service.ServiceScheduler
 import de.vulpescloud.node.terminal.JLineTerminal
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.slf4j.LoggerFactory
@@ -31,47 +33,49 @@ object NodeShutdown : KoinComponent {
         exitProcess(0)
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
     fun commandShutdown() {
-        logger.debug("Stopping ServiceScheduler")
-        ServiceScheduler.cancel()
+        GlobalScope.launch {
+            logger.debug("Stopping ServiceScheduler")
+            ServiceScheduler.cancel()
 
-        logger.debug("Stopping LocalServices")
-        serviceProviderImpl.localServices.forEach { it.sendCommand("stop") }
-        serviceProviderImpl.loggingServices.clear()
+            logger.debug("Stopping LocalServices")
+            serviceProviderImpl.localServices.forEach { it.sendCommand("stop") }
+            serviceProviderImpl.loggingServices.clear()
 
-        logger.info("Waiting for LocalServices to stop")
-        runBlocking {
+            logger.info("Waiting for LocalServices to stop")
             while (serviceProviderImpl.localServices.isNotEmpty()) {
                 logger.debug("Waiting for LocalServices to stop")
                 serviceProviderImpl.localServices.forEach { it.sendCommand("stop") }
                 delay(1000)
             }
+
+
+            logger.debug("Canceling Schedulers")
+            // if (!clusterProvider.localNode().headNode) { ClusterHeartbeatScheduler.instance.cancel()
+            // }
+            // if (clusterProvider.localNode().headNode) {
+            // HeadNodeClusterHeartbeatScheduler.instance.cancel() }
+
+            logger.debug("Deleting Heartbeat")
+            getRC()?.deleteHashField("VULPESCLOUD_NODE_HEARTBEAT", clusterProvider.localNode().name)
+
+            logger.debug("Unloading Modules")
+            moduleProvider.unloadAllModules()
+
+            logger.debug("Shutting down ClusterProvider")
+            val clusterProv = clusterProvider as ClusterProviderImpl
+            clusterProv.shutdown()
+
+            logger.debug("Shutting down DatabaseProvider")
+            databaseProvider.close()
+
+            logger.debug("Shutting down RedisController")
+            getRC()?.shutdown()
+
+            logger.info("Goodbye!")
+            terminal.close()
+            exitProcess(0)
         }
-
-        logger.debug("Canceling Schedulers")
-        // if (!clusterProvider.localNode().headNode) { ClusterHeartbeatScheduler.instance.cancel()
-        // }
-        // if (clusterProvider.localNode().headNode) {
-        // HeadNodeClusterHeartbeatScheduler.instance.cancel() }
-
-        logger.debug("Deleting Heartbeat")
-        getRC()?.deleteHashField("VULPESCLOUD_NODE_HEARTBEAT", clusterProvider.localNode().name)
-
-        logger.debug("Unloading Modules")
-        moduleProvider.unloadAllModules()
-
-        logger.debug("Shutting down ClusterProvider")
-        val clusterProv = clusterProvider as ClusterProviderImpl
-        clusterProv.shutdown()
-
-        logger.debug("Shutting down DatabaseProvider")
-        databaseProvider.close()
-
-        logger.debug("Shutting down RedisController")
-        getRC()?.shutdown()
-
-        logger.info("Goodbye!")
-        terminal.close()
-        exitProcess(0)
     }
 }
