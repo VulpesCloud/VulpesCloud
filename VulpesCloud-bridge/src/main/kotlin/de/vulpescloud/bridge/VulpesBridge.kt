8519076773f727feec396c5494f9542388ced569
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-05-10 11:01:45 +0200

    small Bridge API changes

--- VulpesCloud-bridge/src/main/kotlin/de/vulpescloud/bridge/VulpesBridge.kt
+++ VulpesCloud-bridge/src/main/kotlin/de/vulpescloud/bridge/VulpesBridge.kt
@@ -4,15 +4,13 @@ import de.vulpescloud.api.event.EventManager
 import de.vulpescloud.api.service.ServiceProvider
 import de.vulpescloud.bridge.event.EventManagerImpl
 import de.vulpescloud.bridge.service.ServiceProviderImpl
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
-object VulpesBridge : KoinComponent {
+object VulpesBridge {
 
     private val serviceProvider: ServiceProvider = ServiceProviderImpl()
     private val eventManager: EventManager = EventManagerImpl()
 
     fun getServiceProvider(): ServiceProviderImpl = serviceProvider as ServiceProviderImpl
-    fun getEventManager(): EventManagerImpl = eventManager as EventManagerImpl
 
+    fun getEventManager(): EventManagerImpl = eventManager as EventManagerImpl
 }
