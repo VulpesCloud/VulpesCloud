426d2d4e3bb415f95f59aff911c0e4dc7330b756
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-05-21 15:04:18 +0200

    fix: VirtualConfigReloadChannelListener now checks if a redis message contains a specific string

--- VulpesCloud-api/src/main/kotlin/de/vulpescloud/api/virtualconfig/VirtualConfigReloadChannelListener.kt
+++ VulpesCloud-api/src/main/kotlin/de/vulpescloud/api/virtualconfig/VirtualConfigReloadChannelListener.kt
@@ -4,7 +4,7 @@ import de.vulpescloud.jediswrapper.redis.ChannelListener
 
 class VirtualConfigReloadChannelListener(val config: VirtualConfig) : ChannelListener("VCONFIG_RELOAD_${config.name}") {
     override fun onMessage(message: String) {
-        if (message == "VCONFIG_RELOAD_${config.name}") {
+        if (message.contains("VCONFIG_RELOAD_${config.name}")) {
             config.pull()
         }
     }
