c14e43e58049096015e4af0869bbc79e316d9041
Lukas <113579672+byPixelTV@users.noreply.github.com>
2025-08-21 21:28:43 +0200

    fix: the terminal now displays the log level colors correctly

--- node/src/main/kotlin/de/vulpescloud/node/commands/HelpCommand.kt
+++ node/src/main/kotlin/de/vulpescloud/node/commands/HelpCommand.kt
@@ -32,7 +32,7 @@ class HelpCommand(private val commandProvider: CommandProvider) {
     @Command("help|?")
     fun sendGeneralHelp(source: CommandSource) {
         commandProvider.commands()!!.forEach {
-            source.sendMessage("&f${it.name}${it.aliases} &8- &7${it.description}&8.")
+            source.sendMessage("&f${it.joinNameToAliases(", ")} &8- &7${it.description}&8.")
         }
     }
 
--- node/src/main/kotlin/de/vulpescloud/node/terminal/CompositeConverterImpl.kt
+++ node/src/main/kotlin/de/vulpescloud/node/terminal/CompositeConverterImpl.kt
@@ -3,33 +3,32 @@ package de.vulpescloud.node.terminal
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.pattern.CompositeConverter
-import net.kyori.adventure.text.format.NamedTextColor
 
 class CompositeConverterImpl : CompositeConverter<ILoggingEvent>() {
 
     override fun transform(p0: ILoggingEvent, p1: String): String {
-        return "&" + color(p0.level) + p1
+        return color(p0.level) + p1
     }
 
     private fun color(level: Level): String {
         return when (level.toInt()) {
             Level.INFO_INT -> {
-                NamedTextColor.GREEN.toString()
+                "<color:#4bfb00>"
             }
             Level.WARN_INT -> {
-                NamedTextColor.YELLOW.toString()
+                "<color:#fede00>"
             }
             Level.ERROR_INT -> {
-                NamedTextColor.RED.toString()
+                "<color:#ff0000>"
             }
             Level.TRACE_INT -> {
-                NamedTextColor.LIGHT_PURPLE.toString()
+                "<color:#7F00FF>"
             }
             Level.DEBUG_INT -> {
-                NamedTextColor.LIGHT_PURPLE.toString()
+                "<color:#7F00FF>"
             }
             else -> {
-                NamedTextColor.DARK_GRAY.toString()
+                "<dark_gray>"
             }
         }
     }
