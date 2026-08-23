ea0a16f39f0c6080dd824bd0203060a92612868a
TheCGuyGitHub <thecguy@vulpescloud.de>
2026-08-23 19:52:48 +0200

    try: use println instead of logger.info (idfk why)

--- node/src/main/kotlin/de/vulpescloud/node/templates/TemplateServiceImpl.kt
+++ node/src/main/kotlin/de/vulpescloud/node/templates/TemplateServiceImpl.kt
@@ -139,10 +139,8 @@ class TemplateServiceImpl : TemplateServiceGrpcKt.TemplateServiceCoroutineImplBa
                 TemplateRegistry.matchesLocation(it.location.toDefinition(), request.location)
             }
 
-        logger.info(
-            "listTemplates on {}: {}",
-            Node.instance.configProvider.config.nodeName,
-            templates.joinToString { "${it.id}@${it.location.storageName}-${it.location.nodeName}" },
+        println(
+            "listTemplates on ${Node.instance.configProvider.config.nodeName}: ${templates.joinToString { "${it.id}@${it.location.storageName}-${it.location.nodeName}" }}"
         )
         return ListTemplatesResponse.newBuilder()
             .addAllTemplates(templates.map { it.toDefinition() })
